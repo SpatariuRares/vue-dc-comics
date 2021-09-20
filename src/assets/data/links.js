6d846f03994ccefe5cb21a7cbb8d85317753767c
spata00 <sararares12@gmail.com>
2021-09-20 14:28:14 +0200

    header

---
+++ src/assets/data/links.js
@@ -0,0 +1,77 @@
+    const headerLinks = [
+        {
+            text: "CHARACTERS",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "COMICS",
+            url: "#",
+            current: true,
+        },
+        {
+            text: "MOVIE",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "TV",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "GAMES",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "COLLECTIBLES",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "VIDEOS",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "FANS",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "NEWS",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "SHOP",
+            url: "#",
+            current: false,
+        },
+    ]
+
+    const footerLinks = [
+        {
+            text: "Home",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "Prodotti",
+            url: "#",
+            current: true,
+        },
+        {
+            text: "Chi Siamo",
+            url: "#",
+            current: false,
+        },
+        {
+            text: "Contatti",
+            url: "#",
+            current: false,
+        },
+    ]
+    
+export { headerLinks, footerLinks}
---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="Header">
+    <div class="container">
+      <div class="row d-flex justify-content-between">
+        <div class="col">
+          <img src="@/assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="col">
+          <ul>
+            <li v-for="(link, index) in links" :key="index">
+              <a :class="(link.current) ? 'active' : null" :href="link.url">
+                {{ link.text }}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as Links from '@/assets/data/links.js';
+export default {
+  name: 'Header',
+  data(){
+    return {
+      links: Links.headerLinks
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../style/mixins.scss";
+@import "../style/vars.scss";
+.container{
+  width:80%;
+  margin:10px auto;
+  .row{
+    display:flex;
+    justify-content:space-between;
+    .col{
+      @include center("vertical");
+      align-items:stretch;
+    }
+    ul{
+      display: flex;
+      list-style: none;
+      position: relative;
+      li{ 
+        padding: 0 10px;
+        display: flex;
+        align-items: stretch;
+        a {
+          text-decoration: none;
+          color:black;
+          display: flex;
+          align-items: center;
+          &:hover,
+          &.active{
+            color:$primaryBrand;
+          }
+        }
+        .active{
+          position: relative;
+        }
+        & .active::after{
+          content:"";
+          width:80%;
+          height: 10px;
+          background-color: $primaryBrand;
+          position: absolute;
+          bottom: 0;
+          left: 10%;
+        }
+      }
+    }
+  }
+}
+</style>
